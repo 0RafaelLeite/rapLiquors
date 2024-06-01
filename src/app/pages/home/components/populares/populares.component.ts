d554ba45e18907c537149acf993e9511c6a744c2
pdr-rvr <rovirapedro@icloud.com>
2024-06-01 20:56:14 -0300

    correção de itens da nav-bar e top-page junto a implementação do carousel no component populares

--- src/app/pages/home/components/populares/populares.component.ts
+++ src/app/pages/home/components/populares/populares.component.ts
@@ -6,22 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './populares.component.css'
 })
 export class PopularesComponent  {
-  quantidadeItem1: number = 0;
-  quantidadeItem2: number = 0;
-  quantidadeItem3: number = 0;
-  quantidadeItem4: number = 0;
-  quantidadeItem5: number = 0;
-  incrementarQuantidade(item: number) {
-    this.quantidadeItem1++;
-  }
+  quantities: number[] = [1, 1, 1, 1];
 
-  decrementarQuantidade(item: number) {
-    if (this.quantidadeItem1 > 0) {
-      this.quantidadeItem1--;
-    }
-  }
-
-  adicionarItem(item: number) {
-    console.log(`Adicionando item ${item} - Quantidade: ${this.quantidadeItem1}`);
+  addToCart(product: string, price: number, quantity: number) {
+    console.log(`Adicionado ao carrinho: ${product} - Quantidade: ${quantity} - Preço: R$${price}`);
+    // Aqui você pode adicionar a lógica para realmente adicionar o produto ao carrinho
   }
 }
