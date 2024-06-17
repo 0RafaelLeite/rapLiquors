4b7226f2d4e18c0bef1c8d6a6aa08ad550179958
Siq-Andre <siq.andre2302@gmail.com>
2024-06-16 23:21:00 -0300

    hotfix

---
+++ src/app/itens/backend-item.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Item } from '../models/itemModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendItemService {
+  private apiUrl = 'http://localhost:5000/api/items/save-items'; // Altere para a URL do seu backend
+
+  constructor(private http: HttpClient) { }
+
+  saveItems(items: Item[]): Observable<any> {
+    return this.http.post(this.apiUrl, items);
+  }
+}
